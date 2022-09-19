7dc6e43c54f257fe9920f710c200e76434be5588
fredalbert37 <fredalbert37@outlook.com>
2022-09-18 21:06:26 -0500

    first commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+import { PokemonModule } from './pokemon/pokemon.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname,'..', 'public'),
+    }),
+    PokemonModule
+  ],
 })
 export class AppModule {}
